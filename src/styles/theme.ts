65151883baedaa6848d68bdc8e59053120928549
caiquedegaspari <caique.degaspare@gmail.com>
2023-06-26 09:24:56 -0300

    first commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#665687',
+			light: '#b084cc',
+			dark: '#190933',
+		},
+		secondary: {
+			main: '#190933',
+		},
+		background: {
+			default: '#f1e3e4',
+		},
+		text: {
+			primary: '#fff',
+		},
+	},
+});
